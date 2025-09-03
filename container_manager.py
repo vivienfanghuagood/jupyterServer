c27040e71a768f26bc85b2ea1a4034d50488c6d5
Xun Wang <xunwang2@amd.com>
2025-09-03 15:43:21 +0800

    temp test purpose

--- container_manager.py
+++ container_manager.py
@@ -173,6 +173,7 @@ def start_pod_and_get_jupyter_url() -> tuple[str | None, str | None]:
     token = None
     start = time.time()
     while time.time() - start < timeout:
+        print("Checking if Jupyter server is up…")
         exec_out = stream.stream(
             v1.connect_get_namespaced_pod_exec,
             pod_name,

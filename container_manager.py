528b84cdb39cc7ae928b0975bbaeb56c50bbc605
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2025-09-16 06:19:37 -0700

    Update container_manager.py

--- container_manager.py
+++ container_manager.py
@@ -71,7 +71,7 @@ def start_pod_and_get_jupyter_url() -> tuple[str | None, str | None]:
     "pip install --no-cache-dir jupyter ihighlight && "
     "git clone https://github.com/Mahdi-CV/amd-gpu-workshops && "
     "cd amd-gpu-workshops && cd notebooks && "
-    f"/root/.local/bin/jupyter lab --ip=0.0.0.0 --port={container_port} --allow-root "
+    f"jupyter lab --ip=0.0.0.0 --port={container_port} --allow-root "
     f"--ServerApp.base_url=/jupyter/{pod_name}/ "
     f"--ServerApp.open_browser=False --ServerApp.trust_xheaders=True"
 )
